e94dd4c52ff3a8cb718c2c0fb963f72d92db107c
Richard Killingsworth <richardk80@icloud.com>
2024-06-21 11:57:29 -0500

    Commit 4 made on 06-21-2024 at 11:57:29 AM

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 import robotsTxt from 'astro-robots-txt';
 import eventsource from 'eventsource';
 import compressor from "astro-compressor";
