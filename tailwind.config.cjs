14fe37a4980956d21b6229b9857b6464fcbee1e5
Richard Killingsworth <richardk80@icloud.com>
2024-07-11 16:21:08 -0500

    Commit 11 made on 07-11-2024 at 4:21:08 PM

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,6 +16,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('preline/plugin'),
+    require('preline/plugin')
   ],
 };
