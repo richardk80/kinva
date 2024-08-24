4868cb6418f10658214ae26dc62c44c57e91042e
Richard Killingsworth <richardk80@icloud.com>
2024-08-24 02:01:50 -0500

    Commit 212 made on 08-24-2024 at 2:01:50 AM

---
+++ src/pages/users.js
@@ -0,0 +1,103 @@
+const users = [
+    { 
+      "name": "Dex",
+      "count": 34,
+      "username": "dex", 
+      "loggedin": true, 
+      "profilepic": true, 
+      "twofactor": true,
+      "friendship": true,
+      "profileimg": 'https://i.pravatar.cc/600?img=3',
+      "lastActive": '2024-08-23T10:58:00Z'
+    },
+    { 
+      "name": "Richard",
+      "count": 45,
+      "username": "richard", 
+      "loggedin": false, 
+      "profilepic": true, 
+      "twofactor": false,
+      "requested": true,
+      "profileimg": 'https://i.pravatar.cc/600?img=8',
+      "lastActive": '2024-07-23T09:58:00Z'
+    },
+    { 
+      "name": "Joe",
+      "count": 26,
+      "username": "joe", 
+      "loggedin": true, 
+      "profilepic": true,
+      "twofactor": true,
+      "friendship": true,
+      "profileimg": 'https://i.pravatar.cc/600?img=11',
+      "lastActive": '2024-08-23T08:58:00Z'
+    },
+    { 
+      "name": "Rodney",
+      "count": 18,
+      "username": "rodney", 
+      "loggedin": false, 
+      "profilepic": true, 
+      "twofactor": false,
+      "requested": true,
+      "profileimg": 'https://i.pravatar.cc/600?img=12',
+      "lastActive": '2024-08-23T10:54:00Z'
+    },
+    { 
+      "name": "Keith",
+      "count": 12,
+      "username": "keith", 
+      "loggedin": true, 
+      "profilepic": true, 
+      "twofactor": true,
+      "friendship": true,
+      "profileimg": 'https://i.pravatar.cc/600?img=13',
+      "lastActive": '2024-08-23T07:58:00Z'
+    },
+    { 
+      "name": "James",
+      "count": 52,
+      "username": "james", 
+      "loggedin": false, 
+      "profilepic": true, 
+      "twofactor": false,
+      "requested": true,
+      "profileimg": 'https://i.pravatar.cc/600?img=14',
+      "lastActive": '2024-08-23T08:58:00Z'
+    },
+    { 
+      "name": "Steve",
+      "count": 49,
+      "username": "steve", 
+      "loggedin": true, 
+      "profilepic": true, 
+      "twofactor": true,
+      "friendship": true,
+      "profileimg": 'https://i.pravatar.cc/600?img=18',
+      "lastActive": '2024-08-22T10:58:00Z'
+    },
+    { 
+      "name": "Roger",
+      "count": 8,
+      "username": "roger", 
+      "loggedin": false, 
+      "profilepic": true, 
+      "twofactor": false,
+      "requested": true,
+      "profileimg": 'https://i.pravatar.cc/600?img=53',
+      "lastActive": '2024-08-21T10:58:00Z'
+    },
+    { 
+      "name": "Jay",
+      "count": 39,
+      "username": "jay", 
+      "loggedin": false, 
+      "profilepic": true, 
+      "twofactor": false,
+      "friendship": true,
+      "profileimg": 'https://i.pravatar.cc/600?img=54',
+      "lastActive": '2023-08-24T10:58:00Z'
+    }
+  ];
+
+export default users;
