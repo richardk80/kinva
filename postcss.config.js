8f8459e142a0ce25201581b5c87253133efc68cd
Richard Killingsworth <richardk80@icloud.com>
2024-09-07 22:52:30 -0500

    Commit 262 made on 09-07-2024 at 10:52:30 PM

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,14 @@
+const crypto = require('crypto');
+
 module.exports = {
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-      process.env.NODE_ENV === 'production'
-        ? [
-            require('@fullhuman/postcss-purgecss')({
-              content: ['./src/**/*.astro', './src/**/*.js', './src/**/*.jsx', './src/**/*.tsx', './src/**/*.html'],
-              defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-              safelist: {
-                standard: [/^bg-/, /^text-/],  // You can add classes you want to retain here
-              },
-            }),
-            require('cssnano')({
-              preset: 'default',
-            }),
-          ]
-        : [],
-    ],
-  };  
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('postcss-class-rename')({
+      transform: (class) => {
+        // Hash the class names to a fixed length
+        return crypto.createHash('md5').update(className).digest('hex').slice(0, 6);
+      }
+    }),
+  ],
+};
