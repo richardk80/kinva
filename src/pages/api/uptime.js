adeb9d181716369d7ef48dd0a7f6a248fed46ed5
Richard Killingsworth <richardk80@icloud.com>
2024-10-23 08:06:48 -0500

    Commit 365 made on 10-23-2024 at 8:06:48 AM

--- src/pages/api/uptime.js
+++ src/pages/api/uptime.js
@@ -1,3 +1,5 @@
+export const prerender = false; // Ensure this is not pre-rendered
+
 import axios from 'axios';
 
 // Declare the uptimeData array to store uptime statuses
@@ -38,7 +40,7 @@ const logStatus = (status, explanation = '') => {
 };
 
 // Schedule the status check every 5 minutes (300000 milliseconds)
-setInterval(checkSiteStatus, 300000); // Adjust the interval as needed
+setInterval(checkSiteStatus, 5000); // Adjust the interval as needed
 
 // Run the check immediately when the script starts
 checkSiteStatus();
