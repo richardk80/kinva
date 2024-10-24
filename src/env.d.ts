71c3bf2aee2cec5019f67a9a26b60470d53a89b5
Richard Killingsworth <richardk80@icloud.com>
2024-10-24 02:48:47 -0500

    Commit 376 made on 10-24-2024 at 2:48:47 AM

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly SMTP_USER: string;
+    readonly SMTP_PASS: string;
+    readonly API_KEY: string;
+}
