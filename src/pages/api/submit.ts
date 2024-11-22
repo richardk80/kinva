3771279afbf8772945477f31d7f152e0cae920dc
Richard Killingsworth <richardk80@icloud.com>
2024-11-22 14:29:07 -0600

    Commit 430 made on 11-22-2024 at 2:29:07 PM

--- src/pages/api/submit.ts
+++ src/pages/api/submit.ts
@@ -29,7 +29,7 @@ export const POST: APIRoute = async ({ request }) => {
     await transporter.sendMail({
       from: `"${name}" <${email}>`, // Validate if the SMTP server allows dynamic 'from' addresses
       to: 'contact@himoot.site', // Your recipient address (could be your Gmail)
-      subject: `Kinva ${reason} Message from ${name}`, // Subject line
+      subject: `Kinva ${reason} message from ${name}`, // Subject line
       text: `Name: ${name}\n\nEmail: ${email}\n\nMessage:\n\n${message}`, // Email body
     });
 
