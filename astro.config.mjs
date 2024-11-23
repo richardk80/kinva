dedf2aa95a8df2860517a151a7f0938fc0519411
Richard Killingsworth <richardk80@icloud.com>
2024-11-23 05:47:30 -0600

    Commit 437 made on 11-23-2024 at 5:47:30 AM

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,7 +39,7 @@ export default defineConfig({
     gzip: true,
     brotli: false
   }), icon(), shield({})],
-  output: 'server',
+  output: 'hybrid',
   server: {
     headers: {
       "Accept-Encoding": "gzip, deflate, zstd",
